7a5006439ea630d6ed382deb1e36e68d97926894
SHannon Mulloy & Brain Lau & Michael Summers <student@epicodus.com>
2015-09-03 12:39:22 -0700

    add partials for navbar, edit styling, and add requirements to quest edit page

--- app.rb
+++ app.rb
@@ -21,7 +21,7 @@ get('/login') do
   password = params.fetch("password")
   if User.find_user_login(name, password) != nil
     @user = User.find_user_login(name, password)
-    erb(:character)
+    redirect('/users/' + @user.id.to_s)
   else
     erb(:index)
   end
@@ -36,6 +36,11 @@ post('/login/new') do
     #pass an error message
   end
   @user = User.create({:name => name, :password => password})
+  redirect('/users/' + @user.id.to_s)
+end
+
+get('/users/:user_id') do
+  @user = User.find(params.fetch('user_id').to_i)
   erb(:character)
 end
 
@@ -86,7 +91,7 @@ post('/:user_id/scenes/add') do
   @quest = Quest.find(params.fetch('quest_id').to_i)
   previous_id = params.fetch('previous_scene').to_i
   keyword = params.fetch('keyword')
-  name = params.fetch('name')
+  params.fetch('name') != "" ? name = params.fetch('name') : name = params.fetch('keyword')
   description = params.fetch('description')
   @scene = Scene.create({:name => name, :keyword => keyword, :description => description, :quest_id => @quest.id, :previous_scene => previous_id})
   redirect('/' + @user.id.to_s + '/scenes/' + @scene.id.to_s + '/edit')
@@ -156,13 +161,17 @@ post('/characters/new') do
   erb(:quest)
 end
 
-get('/scenes/:id') do
+get('/characters/:character_id/scenes/:id') do
+  @character = Character.find(params.fetch('character_id').to_i)
+  @user = @character.user
   @scene = Scene.find(params.fetch('id').to_i)
   @@can_continue = @scene.required_observations?
   erb(:scene)
 end
 
-get('/scenes/:id/interpret') do
+get('/characters/:character_id/scenes/:id/interpret') do
+  @character = Character.find(params.fetch('character_id').to_i)
+  @user = @character.user
   destructive_verbs = ['break', 'kick', 'punch', 'hit', 'pick up', 'pick']
   going_verbs = ['go', 'walk', 'jump', 'climb', 'slide', 'somersault', ]
   @scene = Scene.find(params.fetch('id').to_i)
@@ -171,35 +180,37 @@ get('/scenes/:id/interpret') do
     going_verbs.each do |verb|
       if text.downcase.include?(verb) && text.downcase.include?(option.keyword.downcase)
         @@div = ""
-        redirect('/scenes/' + option.id.to_s)
+        redirect('/characters/' + @character.id.to_s + '/scenes/' + option.id.to_s)
       end
     end
   end
   if text.downcase.include?('back')
     @@div = ""
-    redirect('/scenes/' + @scene.previous_scene.to_s)
+    redirect('/characters/' + @character.id.to_s + '/scenes/' + @scene.previous_scene.to_s)
   end
   destructive_verbs.each do |verb|
     if text.downcase.include?(verb)
       @@div = "You can't " + verb + " that."
-      redirect('/scenes/' + @scene.id.to_s)
+      redirect('/characters/' + @character.id.to_s + '/scenes/' + @scene.id.to_s)
     end
   end
   @scene.observations.each do |observation|
     if text.include?(observation.keyword)
-      redirect('/scenes/' + @scene.id.to_s + '/observations/' + observation.id.to_s)
+      redirect('/characters/' + @character.id.to_s + '/scenes/' + @scene.id.to_s + '/observations/' + observation.id.to_s)
     end
   end
   @@div = "You can't do that."
-  redirect('/scenes/' + @scene.id.to_s)
+  redirect('/characters/' + @character.id.to_s + '/scenes/' + @scene.id.to_s)
 end
 
-get('/scenes/:id/observations/:observation_id') do
+get('/characters/:character_id/scenes/:id/observations/:observation_id') do
+  @character = Character.find(params.fetch('character_id').to_i)
+  @user = @character.user
   @scene = Scene.find(params.fetch('id').to_i)
   @observation = Observation.find(params.fetch('observation_id'))
   @@div = @observation.description
   if @observation.required == true && @@can_continue.include?(@observation.id)
     @@can_continue.delete(@observation.id)
   end
-  redirect('/scenes/' + @scene.id.to_s)
+  redirect('/characters/' + @character.id.to_s + '/scenes/' + @scene.id.to_s)
 end
